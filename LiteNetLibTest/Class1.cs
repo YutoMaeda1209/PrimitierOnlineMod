2b50554da9b128dd1e0a4556e1bd053b12ec02db
YutoMaeda <UVTSAs@gmail.com>
2024-01-19 19:41:01 +0900

    Enter litenetlib sample code.

--- LiteNetLibTest/Class1.cs
+++ LiteNetLibTest/Class1.cs
@@ -1,10 +1,64 @@
 ﻿using MelonLoader;
+using System;
+using System.Threading;
 using LiteNetLib;
+using LiteNetLib.Utils;
 
 namespace LiteNetLibTest
 {
     public class Class1: MelonMod
     {
+        public void Server()
+        {
+            EventBasedNetListener listener = new EventBasedNetListener();
+            NetManager server = new NetManager(listener);
+            server.Start(9050 /* port */);
 
+            listener.ConnectionRequestEvent += request =>
+
+            {
+                if (server.ConnectedPeersCount < 10 /* max connections */)
+                    request.AcceptIfKey("SomeConnectionKey");
+                else
+                    request.Reject();
+            };
+
+            listener.PeerConnectedEvent += peer =>
+            {
+                Console.WriteLine("We got connection: {0}", peer); // Show peer ip
+                NetDataWriter writer = new NetDataWriter();        // Create writer class
+                writer.Put("Hello client!");                       // Put some string
+                peer.Send(writer, DeliveryMethod.ReliableOrdered); // Send with reliability
+            };
+
+            while (!Console.KeyAvailable)
+            {
+                server.PollEvents();
+                Thread.Sleep(15);
+            }
+
+            server.Stop();
+        }
+
+        public void Client()
+        {
+            EventBasedNetListener listener = new EventBasedNetListener();
+            NetManager client = new NetManager(listener);
+            client.Start();
+            client.Connect("localhost" /* host ip or name */, 9050 /* port */, "SomeConnectionKey" /* text key or NetDataWriter */);
+            listener.NetworkReceiveEvent += (fromPeer, dataReader, channel, deliveryMethod) =>
+            {
+                Console.WriteLine("We got: {0}", dataReader.GetString(100 /* max length of string */));
+                dataReader.Recycle();
+            };
+
+            while (!Console.KeyAvailable)
+            {
+                client.PollEvents();
+                Thread.Sleep(15);
+            }
+
+            client.Stop();
+        }
     }
 }
