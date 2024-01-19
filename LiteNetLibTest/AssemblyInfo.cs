80e1e01a6d68f1543dd8a727f776791aaf223901
YutoMaeda <UVTSAs@gmail.com>
2024-01-19 10:37:57 +0900

    Add LiteNetLibTest Project.

---
+++ LiteNetLibTest/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using MelonLoader;
+using LiteNetLibTest;
+
+[assembly: MelonInfo(typeof(Class1), "LiteNetLibTest", "1.0.0", "YuchiGames")]
+[assembly: MelonGame("PrimitierDev", "Primitier")]
---
+++ LiteNetLibTest/Class1.cs
@@ -0,0 +1,10 @@
+﻿using MelonLoader;
+using LiteNetLib;
+
+namespace LiteNetLibTest
+{
+    public class Class1: MelonMod
+    {
+
+    }
+}
