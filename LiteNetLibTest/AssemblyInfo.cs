7192a4ca8fcf4528d518ef5b734aef92f8c24970
YutoMaeda <UVTSAs@gmail.com>
2024-01-20 09:56:36 +0900

    Initialized

--- LiteNetLibTest/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using MelonLoader;
-using LiteNetLibTest;
-
-[assembly: MelonInfo(typeof(Class1), "LiteNetLibTest", "1.0.0", "YuchiGames")]
-[assembly: MelonGame("PrimitierDev", "Primitier")]
--- MirrorLibTest/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using MelonLoader;
-using MirrorLibTest;
-
-[assembly: MelonInfo(typeof(Class1), "MirrorLibTest", "1.0.0", "YuchiGames")]
-[assembly: MelonGame("PrimitierDev", "Primitier")]
