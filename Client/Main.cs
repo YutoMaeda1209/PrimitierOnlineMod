93a6bfc2f8a0fe173b9e360b8b05e58d2eb4116f
YuchiGames <UVTSAs@gmail.com>
2024-02-13 23:35:33 +0900

    サンプルコード記述

--- Client/Main.cs
+++ Client/Main.cs
@@ -1,12 +1,40 @@
 ﻿using MelonLoader;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace POMClient
 {
-    public class Main : MelonMod
+    public class Program : MelonMod
     {
-        public Main()
+        UdpClient _udpClient;
+        IPEndPoint _ipEndPoint;
+
+        public static void Main(string[] args)
         {
 
+        }
+
+        public Program()
+        {
+            _udpClient = new UdpClient();
+            _ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11000);
+        }
+
+        public override void OnInitializeMelon()
+        {
+            _udpClient.Connect(_ipEndPoint);
+            receiveWait();
+            Byte[] sendBytes = Encoding.ASCII.GetBytes("Send to server data from client.");
+            _udpClient.Send(sendBytes, sendBytes.Length);
+        }
+
+        Task receiveWait() {
+            while (true)
+            {
+                _udpClient.Receive(ref _ipEndPoint);
+                MelonDebug.Msg(_ipEndPoint.ToString());
+            }
         }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace POMServer
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace POMServer
 {
     public class Program
     {
+        UdpClient _udpClient;
+        IPEndPoint _ipEndPoint;
+
         public static void Main(string[] args)
         {
 
+        }
+
+        public Program()
+        {
+            _udpClient = new UdpClient();
+            _ipEndPoint = new IPEndPoint(IPAddress.Any, 11000);
+            receiveWait();
+        }
+
+        Task receiveWait()
+        {
+            while (true)
+            {
+                _udpClient.Receive(ref _ipEndPoint);
+                Console.Write(_ipEndPoint.ToString());
+                Byte[] sendBytes = Encoding.ASCII.GetBytes("Send to client data from server.");
+                _udpClient.Send(sendBytes, sendBytes.Length, _ipEndPoint);
+            }
         }
     }
 }
