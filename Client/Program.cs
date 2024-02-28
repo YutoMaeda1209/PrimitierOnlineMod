7dbff1a230842cdcbbb7928666ced03dbe894502
YutoMaeda <UVTSAs@gmail.com>
2024-02-29 02:14:13 +0900

    一旦削除（ソケット通信に切り替えるため）

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,37 +1,12 @@
 ﻿using MelonLoader;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
 
 namespace POMClient
 {
     public class Program : MelonMod
     {
-        UdpClient _udpClient;
-        IPEndPoint _ipEndPoint;
-        Thread _receiveThread;
-
         public Program()
         {
-            _udpClient = new UdpClient();
-            _ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11000);
-        }
-
-        public override void OnLateInitializeMelon()
-        {
-            _udpClient.Connect(_ipEndPoint);
-            _receiveThread = new Thread(new ThreadStart(receiveWait));
-            _receiveThread.Start();
-            Byte[] sendBytes = Encoding.ASCII.GetBytes("Send to server data from client.");
-            _udpClient.SendAsync(sendBytes, sendBytes.Length);
-        }
-
-        void receiveWait() {
-            while (true)
-            {
-                Byte[] receiveBytes = _udpClient.Receive(ref _ipEndPoint);
-                LoggerInstance.Msg(Encoding.ASCII.GetString(receiveBytes));
-            }
+            
         }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,16 +1,7 @@
-﻿using System.Net;
-using System.Net.Sockets;
-using System.Text;
-
-namespace POMServer
+﻿namespace POMServer
 {
     public class Program
     {
-        UdpClient _udpClient;
-        IPEndPoint _ipEndPoint;
-        Thread _receiveThread;
-        IPEndPoint[] _iPEndPoints;
-
         public static void Main(string[] args)
         {
             new Program();
@@ -18,21 +9,7 @@ namespace POMServer
 
         public Program()
         {
-            _udpClient = new UdpClient(11000);
-            _ipEndPoint = new IPEndPoint(IPAddress.Any, 11000);
-            _receiveThread = new Thread(new ThreadStart(receiveWait));
-            _receiveThread.Start();
-        }
 
-        void receiveWait()
-        {
-            while (true)
-            {
-                Byte[] receiveBytes = _udpClient.Receive(ref _ipEndPoint);
-                Console.WriteLine(Encoding.ASCII.GetString(receiveBytes) + _ipEndPoint.Address.ToString());
-                Byte[] sendBytes = Encoding.ASCII.GetBytes("Send to client data from server.");
-                _udpClient.SendAsync(sendBytes, sendBytes.Length, _ipEndPoint);
-            }
         }
     }
 }
