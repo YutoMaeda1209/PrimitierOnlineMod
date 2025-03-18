3e76e0c81f9fac5ebda869efde97e2f3de82bd42
kirimine170 <kirimine170@gmail.com>
2025-03-19 01:50:56 +0900

    :recycle: update AssemblyInfo.cs
    移動と霧峰の名前追加

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,8 +1,8 @@
 using System.Runtime.InteropServices;
 using MelonLoader;
-using YuchiGames.POM.Client;
+using YuchiGames.POM;
 
 [assembly: ComVisible(false)]
 [assembly: Guid("342a9c33-d50e-418e-982e-83007bddd630")]
-[assembly: MelonInfo(typeof(Program), "PrimitierOnlineMod", "0.1.0", "YuchiGames")]
+[assembly: MelonInfo(typeof(Program), "PrimitierOnlineMod", "0.1.0", "YuchiGames, kirimine170")]
 [assembly: MelonGame("PrimitierDev", "Primitier")]
