7d03233b2461c352ce39c8ad5389f03189ec0d61
kirimine170 <kirimine170@gmail.com>
2025-03-19 21:42:24 +0900

    :pencil2: update Program.cs
    brokerにworldSeedを送るまででけた

--- Program.cs
+++ Program.cs
@@ -9,23 +9,43 @@ using System.Security.Authentication;
 using Microsoft.Extensions.Configuration;
 using UnityEngine;
 using Il2Cpp;
-
+using YuchiGames.POM.Network.Mqtt;
+using System.Text.Json;
 
 
 namespace YuchiGames.POM
 {
     public class Program : MelonMod
     {
-        public override void OnInitializeMelon()
+        private IConfiguration _configuration;
+        private string TOPIC = "world/seed";
+        MqttManager mqttManager;
+        public override async void OnInitializeMelon()
         {
-            
+            _configuration = new ConfigurationBuilder()
+                .AddJsonFile($"{Directory.GetCurrentDirectory()}/Mods/config.json", true, false)
+                .Build();
+            string MQTT_SERVER = _configuration["Mqtt:Server"];
+            int MQTT_PORT = int.Parse(_configuration["Mqtt:Port"]);
+            string MQTT_USERNAME_A = _configuration["Mqtt:A:Username"];
+            string MQTT_PASSWORD_A = _configuration["Mqtt:A:Password"];
+            string MQTT_CLIENT_ID_A = _configuration["Mqtt:A:ClientId"]; 
+
+            mqttManager = new MqttManager(MQTT_SERVER, MQTT_PORT, MQTT_CLIENT_ID_A, MQTT_USERNAME_A, MQTT_PASSWORD_A, true);
+            await mqttManager.ConnectAsync();
         }
 
-        public override void OnUpdate()
+        public override async void OnUpdate()
         {
             if (Input.GetKeyDown(KeyCode.F1))
             {
-                int hoge = TerrainGenerator.worldSeed;
+                if (mqttManager == null)
+                {
+                    MelonLogger.Error("mqttManager is null!");
+                    return;
+                }
+                int worldSeed = TerrainGenerator.worldSeed;
+                await mqttManager.PublishAsync(TOPIC, worldSeed.ToString(), 2, true);
             }
         }
 
