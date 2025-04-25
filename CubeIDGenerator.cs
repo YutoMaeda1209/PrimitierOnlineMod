a629f81fd5ea6ee4f161867cbc3578473ac90690
kirimine170 <kirimine170@gmail.com>
2025-04-26 07:11:39 +0900

    :pencil2: update CubeIDGenerator.cs
    hashをbyteで管理するように変更

--- CubeIDGenerator.cs
+++ CubeIDGenerator.cs
@@ -14,17 +14,21 @@ namespace YuchiGames.POM
     public static class CubeBaseIDGenerator
     {
         private static Dictionary<string, int> counters = new Dictionary<string, int>();
-        public static string GenerateID(CubeBase cube)
+        public static byte[] GenerateID(CubeBase cube)
         {
             Vector3 pos = cube.transform.position;
             Vector2Int chunk = CubeGenerator.WorldToChunkPos(pos);
             string id = chunk.x + ":" + chunk.y;
+            byte[] hashBytes = new byte[32];
             using (HMACSHA256 hmac = new HMACSHA256(Encoding.UTF8.GetBytes(id)))
             {
-                byte[] hashBytes = hmac.ComputeHash(Encoding.UTF8.GetBytes(""+pos.x+pos.y+pos.z));
-                id = BitConverter.ToString(hashBytes).Replace("-", String.Empty);
+                hashBytes = hmac.ComputeHash(Encoding.UTF8.GetBytes(""+pos.x+pos.y+pos.z));
             }
-            return id;
+            StringBuilder sb = new StringBuilder(hashBytes.Length * 2);
+            foreach(byte b in hashBytes)
+                sb.Append(b.ToString("X2"));
+            MelonLogger.Msg(sb.ToString());
+            return hashBytes;
         }
     }
 }
