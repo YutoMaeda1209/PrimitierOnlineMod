4cc6ad50692d14fccd62fc3be105e5a5bc5ec6da
kirimine170 <kirimine170@gmail.com>
2025-04-30 21:20:01 +0900

    :recycle: CubeIDGenerator.cs

--- Hooks/CubeIDGenerator.cs
+++ Hooks/CubeIDGenerator.cs
@@ -1,14 +1,14 @@
 using System;
 using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using Il2Cpp;
 using MelonLoader;
 using Unity.XR.CoreUtils;
 using UnityEngine;
-using System.Text;
 
-namespace YuchiGames.POM
+namespace YuchiGames.POM.Hooks
 {
 
     public static class CubeBaseIDGenerator
@@ -18,14 +18,14 @@ namespace YuchiGames.POM
         {
             Vector3 pos = cube.transform.position;
             Vector2Int chunk = CubeGenerator.WorldToChunkPos(pos);
-            string id = chunk.x + ":" + chunk.y;
+            string id = $"{chunk.x}{chunk.y}";
             byte[] hashBytes = new byte[32];
             using (HMACSHA256 hmac = new HMACSHA256(Encoding.UTF8.GetBytes(id)))
             {
-                hashBytes = hmac.ComputeHash(Encoding.UTF8.GetBytes(""+pos.x+pos.y+pos.z));
+                hashBytes = hmac.ComputeHash(Encoding.UTF8.GetBytes($"{pos.x}{pos.y}{pos.z}"));
             }
             StringBuilder sb = new StringBuilder(hashBytes.Length * 2);
-            foreach(byte b in hashBytes)
+            foreach (byte b in hashBytes)
                 sb.Append(b.ToString("X2"));
             MelonLogger.Msg(sb.ToString());
             return hashBytes;
