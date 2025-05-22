b06f8f3e2c703946996acc50ef935a7c0b186120
kirimine170 <kirimine170@gmail.com>
2025-05-23 01:02:04 +0900

    :pencil2: update Program.cs
    コンフィグファイルの構造を修正
    
    (cherry picked from commit 9e45a445686bbfdbbd7198684f516d001e0a79fc)

--- Program.cs
+++ Program.cs
@@ -63,9 +63,9 @@ namespace YuchiGames.POM
                     .Build();
                 string MQTT_SERVER = _configuration["Mqtt:Server"];
                 int MQTT_PORT = int.Parse(_configuration["Mqtt:Port"]);
-                string MQTT_USERNAME_A = _configuration["Mqtt:A:Username"];
-                string MQTT_PASSWORD_A = _configuration["Mqtt:A:Password"];
-                string MQTT_CLIENT_ID_A = _configuration["Mqtt:A:ClientId"];
+                string MQTT_USERNAME_A = _configuration["Mqtt:kirimine170_A:Username"];
+                string MQTT_PASSWORD_A = _configuration["Mqtt:kirimine170_A:Password"];
+                string MQTT_CLIENT_ID_A = _configuration["Mqtt:kirimine170_A:ClientId"];
 
                 mqttManager = new MqttManager(MQTT_SERVER, MQTT_PORT, MQTT_CLIENT_ID_A, MQTT_USERNAME_A, MQTT_PASSWORD_A, true);
                 await mqttManager.ConnectAsync();
