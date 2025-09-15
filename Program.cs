56cc25e6227042831580f577f4456fb70b3e3c2b
YutoMaeda <UVTSAs@gmail.com>
2025-09-16 03:46:03 +0900

    ✏️ update Program.cs
    今のデバッグで必要のないメソッド等をコメントアウト

--- Program.cs
+++ Program.cs
@@ -1,20 +1,9 @@
-using System;
-using System.Collections;
-using System.Security.Authentication;
-using System.Text;
-using System.Text.Json;
-using System.Threading;
-using System.Threading.Tasks;
-using HarmonyLib;
 using Il2Cpp;
 using MelonLoader;
 using Microsoft.Extensions.Configuration;
-using MQTTnet;
-using MQTTnet.Client;
-using MQTTnet.Formatter;
-using MQTTnet.Server;
 using UnityEngine;
 using YuchiGames.POM.Hooks;
+using YuchiGames.POM.Instance;
 using YuchiGames.POM.Network.Mqtt;
 using YuchiGames.POM.Test;
 
@@ -23,114 +12,155 @@ namespace YuchiGames.POM
 {
     public class Program : MelonMod
     {
-        private Dictionary<KeyCode, Func<Task>> keyActions;
-        private IConfiguration _configuration;
-        private string worldSeedTopic = "world/seed";
-        private string playerTopic = "player/0/transform"; // ユーザ認証を作成した場合には、0の部分に{player_id}が実装されます。
-        private bool isPlayerSynchronized = false;
-        private GameObject player;
+        public static bool IsDebugMsg { get; set; } = false;
+        public static MqttOptions? MqttOptions => s_mqttOptions;
 
-        MqttManager mqttManager;
+        private Dictionary<KeyCode, Func<Task>>? _keyActions;
+        private static MqttOptions? s_mqttOptions;
 
         public override void OnEarlyInitializeMelon()
         {
-            keyActions = new Dictionary<KeyCode, Func<Task>>
+            _keyActions = new Dictionary<KeyCode, Func<Task>>
             {
-                {KeyCode.F1, HandleF1Async},
-                {KeyCode.F2, HandleF2Async},
-                {KeyCode.F3, HandleF3Async},
-                {KeyCode.F4, HandleF4Async},
-                {KeyCode.F5, HandleF5Async},
-                {KeyCode.F6, HandleGenerateFromBinaryAsync},
-                {KeyCode.F7, GenerateCubeBinaryAsync},
+                { KeyCode.BackQuote, SwitchDebugMsg },
+                { KeyCode.F1, RegisterWorldSeed },
+                { KeyCode.F2, EnterInstance },
+                { KeyCode.F3, LeaveInstance },
+                { KeyCode.F5, GenerateCubeByCubeGenerator },
+                { KeyCode.F6, HandleGenerateFromBinaryAsync },
+                { KeyCode.F7, GenerateCubeBinaryAsync },
             };
-        }
 
-        public override async void OnInitializeMelon()
-        {
-            _configuration = new ConfigurationBuilder()
+            IConfiguration configuration = new ConfigurationBuilder()
                 .AddJsonFile($"{Directory.GetCurrentDirectory()}/Mods/config.json", true, false)
                 .Build();
-            string MQTT_SERVER = _configuration["Mqtt:Server"];
-            int MQTT_PORT = int.Parse(_configuration["Mqtt:Port"]);
-            string MQTT_USERNAME_A = _configuration["Mqtt:A:Username"];
-            string MQTT_PASSWORD_A = _configuration["Mqtt:A:Password"];
-            string MQTT_CLIENT_ID_A = _configuration["Mqtt:A:ClientId"];
+            s_mqttOptions = new MqttOptions(
+                configuration["Mqtt:Server"],
+                int.Parse(configuration["Mqtt:Port"]),
+                configuration["Mqtt:YuchiGames:ClientId"],
+                configuration["Mqtt:YuchiGames:Username"],
+                configuration["Mqtt:YuchiGames:Password"],
+                true
+            );
+        }
 
-            mqttManager = new MqttManager(MQTT_SERVER, MQTT_PORT, MQTT_CLIENT_ID_A, MQTT_USERNAME_A, MQTT_PASSWORD_A, true);
-            await mqttManager.ConnectAsync();
+        // public override void OnInitializeMelon()
+        // {
+        //     _configuration = new ConfigurationBuilder()
+        //         .AddJsonFile($"{Directory.GetCurrentDirectory()}/Mods/config.json", true, false)
+        //         .Build();
+        //     MqttOptions = new MqttOptions(
+        //         _configuration["Mqtt:Server"] ?? "localhost",
+        //         int.Parse(_configuration["Mqtt:Port"] ?? "1883"),
+        //         _configuration["Mqtt:YuchiGames_A:ClientId"] ?? "POM_Client",
+        //         _configuration["Mqtt:YuchiGames_A:Username"] ?? "username",
+        //         _configuration["Mqtt:YuchiGames_A:Password"] ?? "password"
+        //     );
+        // }
 
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
             WorldLauncher.Instance = new WorldLauncher();
         }
 
         public override async void OnUpdate()
         {
-            if (mqttManager == null)
+            if (_keyActions == null)
             {
-                MelonLogger.Error("mqttManager is null in OnUpdate!");
+                MelonLogger.Error("keyActions is null");
                 return;
             }
-            if (keyActions == null) MelonLogger.Error("keyactions is null");
-            foreach (var entry in keyActions)
+            foreach (var entry in _keyActions)
             {
                 if (Input.GetKeyDown(entry.Key))
                     await entry.Value();
             }
-            if (isPlayerSynchronized)
-            {
-                await mqttManager.PublishAsync(playerTopic, TransformSerializer.TransformToBytes(player.transform), 0, false);
-            }
         }
 
-        private async Task HandleF1Async()
+        public async Task SwitchDebugMsg()
         {
-            int worldSeed = TerrainGenerator.worldSeed;
-            await mqttManager.PublishAsync(worldSeedTopic, worldSeed.ToString(), 2, true);
-        }
-
-        private async Task HandleF2Async()
-        {
-            await mqttManager.RegisterCallbackAndSubscribeAsync(worldSeedTopic, 2, (topic, payload) =>
-            {
-                string seedText = Encoding.UTF8.GetString(payload);
-                MelonLogger.Msg($"Received on {topic}: {seedText}");
-                MelonCoroutines.Start(WorldLauncher.Instance.ProcessSeedMessageCoroutine(topic, seedText));
-            });
-        }
-
-        private async Task HandleF3Async()
-        {
-            if (player == null)
-                player = GameObject.Find("Player/XR Origin");
-            isPlayerSynchronized = true;
+            IsDebugMsg = !IsDebugMsg;
+            MelonLogger.Msg($"Debug message is {(IsDebugMsg ? "enabled" : "disabled")}.");
             await Task.CompletedTask;
         }
 
-        private async Task HandleF4Async()
+        public async Task RegisterWorldSeed()
         {
-            if (player == null)
-                player = GameObject.Find("Player/XR Origin");
-            await mqttManager.RegisterCallbackAndSubscribeAsync(playerTopic, 0, (topic, payload) =>
-            {
-                MelonLogger.Msg($"Received on {topic}: {BitConverter.ToString(payload)}");
-                MelonCoroutines.Start(UpdatePlayerTransformCoroutine(payload));
-            });
+            if (MqttOptions == null)
+                throw new NullReferenceException("MqttOptions is null");
+            MqttManager? mqttManager = new MqttManager(MqttOptions);
+            await mqttManager.ConnectAsync();
+
+            System.Random random = new System.Random();
+            string seed = random.Next(int.MinValue, int.MaxValue).ToString();
+            await mqttManager.PublishAsync("world/seed", seed, 2, true);
+
+            await mqttManager.DisconnectAsync();
         }
 
-        private async Task HandleF5Async()
+        public async Task EnterInstance()
+        {
+            InstanceManager.Instance = new InstanceManager();
+            await InstanceManager.Instance.EnterInstance();
+        }
+
+        public async Task LeaveInstance()
+        {
+            if (InstanceManager.Instance == null)
+                return;
+            await InstanceManager.Instance.LeaveInstance();
+            InstanceManager.Instance = null;
+        }
+
+        // private async Task HandleF1Async()
+        // {
+        //     int worldSeed = TerrainGenerator.worldSeed;
+        //     await mqttManager.PublishAsync(worldSeedTopic, worldSeed.ToString(), 2, true);
+        // }
+
+        // private async Task HandleF2Async()
+        // {
+        //     await mqttManager.RegisterCallbackAndSubscribeAsync(worldSeedTopic, 2, (topic, payload) =>
+        //     {
+        //         string seedText = Encoding.UTF8.GetString(payload);
+        //         MelonLogger.Msg($"Received on {topic}: {seedText}");
+        //         MelonCoroutines.Start(WorldLauncher.Instance.ProcessSeedMessageCoroutine(topic, seedText));
+        //     });
+        // }
+
+        // private async Task HandleF3Async()
+        // {
+        //     if (player == null)
+        //         player = GameObject.Find("Player/XR Origin");
+        //     isPlayerSynchronized = true;
+        //     await Task.CompletedTask;
+        // }
+
+        // private async Task HandleF4Async()
+        // {
+        //     if (player == null)
+        //         player = GameObject.Find("Player/XR Origin");
+        //     await mqttManager.RegisterCallbackAndSubscribeAsync(playerTopic, 0, (topic, payload) =>
+        //     {
+        //         MelonLogger.Msg($"Received on {topic}: {BitConverter.ToString(payload)}");
+        //         MelonCoroutines.Start(UpdatePlayerTransformCoroutine(payload));
+        //     });
+        // }
+
+        private async Task GenerateCubeByCubeGenerator()
         {
             CubeGenerator.GenerateCube(new Vector3(130, 10, 130), new Quaternion(), new Vector3(0.5f, 0.5f, 0.5f), Substance.Stone, CubeAppearance.SectionState.Right, new CubeAppearance().uvOffset, "");
         }
 
-        private IEnumerator UpdatePlayerTransformCoroutine(byte[] payload)
-        {
-            // 1フレーム待つことで確実にメインスレッド上で実行
-            yield return null;
-            if (player == null)
-                player = GameObject.Find("Player/XR Origin");
-            TransformSerializer.BytesToTransform(payload, player.transform);
-            MelonLogger.Msg(player.transform.ToString());
-        }
+        // private IEnumerator UpdatePlayerTransformCoroutine(byte[] payload)
+        // {
+        //     // 1フレーム待つことで確実にメインスレッド上で実行
+        //     yield return null;
+        //     if (player == null)
+        //         player = GameObject.Find("Player/XR Origin");
+        //     TransformSerializer.BytesToTransform(payload, player.transform);
+        //     MelonLogger.Msg(player.transform.ToString());
+        // }
 
         private async Task GenerateCubeBinaryAsync()
         {
