617f39f46832bef640376ba1cfbda3c686460cca
YutoMaeda <UVTSAs@gmail.com>
2025-09-16 03:36:22 +0900

    ✏️ update Avatar.cs

--- Hooks/Avatar.cs
+++ Hooks/Avatar.cs
@@ -1,10 +1,18 @@
+using System.Collections;
 using Il2Cpp;
+using MelonLoader;
 using UnityEngine;
 
 namespace YuchiGames.POM.Hooks
 {
     public class Avatar
     {
+        public string Id { get { return _id; } }
+        public Transform? AvatarTransform { get { return _avatarTransform; } }
+        public Transform? HeadTransform { get { return _headTransform; } }
+        public Transform? LeftTransform { get { return _leftTransform; } }
+        public Transform? RightTransform { get { return _rightTransform; } }
+
         private static GameObject s_proxyHeadObject;
         private static GameObject s_proxyLeftObject;
         private static GameObject s_proxyRightObject;
@@ -14,19 +22,21 @@ namespace YuchiGames.POM.Hooks
         private GameObject? _headObject;
         private GameObject? _leftObject;
         private GameObject? _rightObject;
-
-        public string Id { get { return _id; } }
+        private Transform? _avatarTransform;
+        private Transform? _headTransform;
+        private Transform? _leftTransform;
+        private Transform? _rightTransform;
 
         static Avatar()
         {
             AvatarVisibility avatarVisibility = GameObject.FindObjectOfType<AvatarVisibility>();
             if (avatarVisibility != null)
             {
-                s_proxyHeadObject = avatarVisibility.proxyHead;
+                s_proxyHeadObject = GameObject.Instantiate(avatarVisibility.proxyHead);
                 s_proxyHeadObject.name = "ProxyHead";
-                s_proxyLeftObject = avatarVisibility.proxyLeftHand;
+                s_proxyLeftObject = GameObject.Instantiate(avatarVisibility.proxyLeftHand);
                 s_proxyLeftObject.name = "ProxyLeftHand";
-                s_proxyRightObject = avatarVisibility.proxyRightHand;
+                s_proxyRightObject = GameObject.Instantiate(avatarVisibility.proxyRightHand);
                 s_proxyRightObject.name = "ProxyRightHand";
             }
             else
@@ -39,18 +49,22 @@ namespace YuchiGames.POM.Hooks
         {
             _id = id;
             _avatarObject = new GameObject($"Avatar_{id}");
+            _avatarTransform = _avatarObject.transform;
 
             _headObject = GameObject.Instantiate(s_proxyHeadObject);
             _headObject.transform.GetChild(0).gameObject.layer = LayerMask.NameToLayer("Default");
             _headObject.transform.SetParent(_avatarObject.transform, false);
+            _headTransform = _headObject.transform;
 
             _leftObject = GameObject.Instantiate(s_proxyLeftObject);
             GameObject.Destroy(_leftObject.GetComponent<ProxyFingerController>());
             _leftObject.transform.SetParent(_avatarObject.transform, false);
+            _leftTransform = _leftObject.transform;
 
             _rightObject = GameObject.Instantiate(s_proxyRightObject);
             GameObject.Destroy(_rightObject.GetComponent<ProxyFingerController>());
             _rightObject.transform.SetParent(_avatarObject.transform, false);
+            _rightTransform = _rightObject.transform;
         }
 
         public enum AvatarPartType
@@ -62,6 +76,8 @@ namespace YuchiGames.POM.Hooks
 
         public void SetPartTransform(AvatarPartType avatarPartType, Vector3 position, Quaternion rotation)
         {
+            Melon<Program>.Logger.Msg("SetPartTransform called.");
+
             switch (avatarPartType)
             {
                 case AvatarPartType.Head:
